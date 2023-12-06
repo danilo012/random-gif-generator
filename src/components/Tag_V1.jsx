1b0ea0b45d59194935fb7c040fa584a5154fe6ac
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-06 11:38:17 +0800

    Update Tag_V1.jsx

--- src/components/Tag_V1.jsx
+++ src/components/Tag_V1.jsx
@@ -5,10 +5,10 @@ const Tag = () =>{
     const {gif, fetchImg} = Common()
     return (
     <div className="container">
-        <h1>Random Tag Image</h1>
+       
         <img src={gif} alt="Random Img" />
         <input type="text"  value={tag} onChange={(e)=>setTag(e.target.value)}/>
-        <button onClick={()=>fetchImg(tag)}>Change</button>
+        <button onClick={()=>fetchImg(tag)}></button>
     </div>
     )
 }
