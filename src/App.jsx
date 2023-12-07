aa5d46c49921803d64306bfb27ec862ed5501d8a
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-07 11:36:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import Random from './components/Random_V1'
-import Tag from './components/Tag_V1'
 function App() {
 
   return (
@@ -8,7 +7,7 @@ function App() {
     
     <div className="main-container">
       <Random/>
-      <Tag/>
+      
     </div>
     </>
   )
